2f999ac4c0c05600f6c9b99ada770e61e551063c
dmartosl <daniel.martos.sspa@gmail.com>
2016-11-11 19:04:34 +0100

    Factory method in Entity

--- entity.py
+++ entity.py
@@ -13,16 +13,23 @@ class Entity:
     """
 
     def __init__(self):
-        self._id = uuid.uuid4().hex
+        """Initialize a Entity.
 
+        Do NOT call directly. Use the factory method.
+        """
+        pass
 
     def __eq__(self, ent):
         return self._id == ent.id
-
 
     @property
     def id(self):
         """A string unique identifier for the entity."""
         return self._id
 
-
+    @staticmethod
+    def factory():
+        """Factory method."""
+        obj = Entity()
+        obj._id = uuid.uuid4().hex
+        return obj
